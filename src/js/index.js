80895f1eb3dcd083dead905e6371fc33ecf41d29
Bryan Moreno <abre.go@outlook.com>
2021-09-27 20:00:20 -0500

    fix buttons

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,103 @@
 import '../css/styles.css';
 import pokemon from '../assets/pokeDefault.png';
 
-document.querySelector('#pokemonImg').src = pokemon;
-
-
-
-//Accordion
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "flex") {
-      panel.style.display = "none";
-      this.children[0].innerText = "expand_more";
-    } else {
-      panel.style.display = "flex";
-      this.children[0].innerText = "expand_less";
-    }
-  });
+function getPokemon(id) {
+    const apiUrl = new URL(`https://pokemon-bedu.herokuapp.com/v1/pokemons/${id}`);
+    return fetch(apiUrl)
+        .then(response => response.json());
 }
 
-//Falta que se cierren los demás al abrir otro
-//Al volver a cargar la página se abren todos idk why
+function getGeneration(id) {
+    const apiUrl = new URL(`https://pokemon-bedu.herokuapp.com/v1/gens/${id}`);
+    return fetch(apiUrl)
+        .then(response => response.json());
+}
+
+function populatePokemonData(id) {
+    const imageControl = document.getElementById('pokemonImg');
+    const nameControl = document.getElementById('nameInfo');
+    const pokedexNumberControl = document.getElementById('numInfo');
+    const genControl = document.getElementById('genInfo');
+    const categoryControl = document.getElementById('catInfo');
+    const typesContainer = document.getElementById('types');
+    const abilitiesContainer = document.getElementById('abilities');
+
+    getPokemon(id).then(pokemon => {
+        // Image
+        imageControl.src = pokemon.imageUrl;
+        // Name
+        setTextChild(nameControl, pokemon.name);
+        // Pokedex number
+        setTextChild(pokedexNumberControl, pokemon.pokedexNumber ?? '#');
+        // Generation
+        getGeneration(pokemon.gen).then(gen => {
+            const generationText = `${pokemon.gen} - ${gen.name}`
+            setTextChild(genControl, generationText ?? '#');
+        })
+        // Classification
+        setTextChild(categoryControl, pokemon.classification);
+        // Types
+        setTextArrayChild(typesContainer, pokemon.types, 'typeInfo');
+        // Abilities
+        setTextArrayChild(abilitiesContainer, pokemon.abilities, 'pokemonData');
+    });
+}
+
+function setTextChild(parentComponent, text) {
+    const textNode = document.createTextNode(text);
+    // Remove children if children exist
+    if (parentComponent.hasChildNodes()) {
+        deleteChildren(parentComponent);
+    }
+    // Add text
+    parentComponent.appendChild(textNode);
+}
+
+function setTextArrayChild(parentComponent, textArray, className) {
+    // Remove children if children exist
+    if (parentComponent.hasChildNodes()) {
+        deleteChildren(parentComponent);
+    }
+
+    // Add each text in array
+    for(const text of textArray) {
+        const textNode = document.createTextNode(text);
+        const paragraphElement = document.createElement('p');
+
+        // Nest text in paragraph
+        paragraphElement.appendChild(textNode);
+        paragraphElement.className = className;
+        
+        // Add paragraph
+        parentComponent.appendChild(paragraphElement);
+    }
+}
+
+function deleteChildren(parentComponent) {
+    while (parentComponent.firstChild) {
+        parentComponent.removeChild(parentComponent.firstChild);
+    }
+}
+
+function setButtonsBehavior() {
+    const prevButton = document.getElementById('btnPrev');
+    prevButton.addEventListener('click', prevPokemon);
+    const nextButton = document.getElementById('btnNext');
+    nextButton.addEventListener('click', nextPokemon);
+}
+
+function nextPokemon() {
+    if(currentPokemonId < 150) {
+        populatePokemonData(++currentPokemonId);
+    }
+}
+
+function prevPokemon() {
+    if(currentPokemonId > 1) {
+        populatePokemonData(--currentPokemonId);
+    }
+}
+
+var currentPokemonId = 1;
+setButtonsBehavior();
+populatePokemonData(currentPokemonId);
--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,48 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/js/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './src/index.html'
-    })
-  ],
-  module: {
-    rules: [
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ['@babel/preset-env']
-          }
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+        ]
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
         }
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i, 
-        type: 'asset/resource',
-      },
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
-    ]
-  },
-  devServer: {
-    static: path.resolve(__dirname, 'dist')
-  }
+    }
 }
 
 
