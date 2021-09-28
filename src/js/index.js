0aac36b10e03ec4e2d5cd3ee5b298b08a3563d83
Maria Fernanda <mafer13cp@gmail.com>
2021-09-27 21:02:17 -0600

    acordion ya abre y cierra

--- src/js/index.js
+++ src/js/index.js
@@ -210,3 +210,22 @@ let currentPokemonId = 1;
 setButtonsBehavior();
 populatePokemonFromAPI(currentPokemonId);
 populateSideBar();
+
+//Accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "flex") {
+      panel.style.display = "none";
+      this.children[0].innerText = "expand_more";
+    } else {
+      panel.style.display = "flex";
+      this.children[0].innerText = "expand_less";
+    }
+  });
+}
